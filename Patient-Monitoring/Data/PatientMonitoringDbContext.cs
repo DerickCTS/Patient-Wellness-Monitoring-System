487ed36af0c6d95b700407504009505db81197a3
Derick Davies <derick.davies@cognizant.com>
2025-09-22 22:12:31 +0530

    first commit

---
+++ Patient-Monitoring/Data/PatientMonitoringDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Patient_Monitoring.Data
+{
+    public class PatientMonitoringDbContext : DbContext
+    {
+        public PatientMonitoringDbContext(DbContextOptions<PatientMonitoringDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Define relationships and constraints here if needed...
+
+        }
+    }
+}
+
+
+
